9578fef02e4c199e5b8a33e37255a2e189d6b9d0
Илюша <79216023478@yandex.ru>
2022-10-27 12:37:55 +0300

    feat: (ewm-server) добавить short-dto для юзера

--- ewm-server/src/main/java/ru/practicum/explore/with/me/user/UserMapper.java
+++ ewm-server/src/main/java/ru/practicum/explore/with/me/user/UserMapper.java
@@ -1,6 +1,7 @@
 package ru.practicum.explore.with.me.user;
 
 import ru.practicum.explore.with.me.user.dto.UserDto;
+import ru.practicum.explore.with.me.user.dto.UserShortDto;
 import ru.practicum.explore.with.me.user.model.User;
 
 public class UserMapper {
@@ -16,6 +17,13 @@ public class UserMapper {
                 .id(user.getId())
                 .email(user.getEmail())
                 .name((user.getName()))
+                .build();
+    }
+
+    public static UserShortDto userShortDto(User user) {
+        return UserShortDto.builder()
+                .id(user.getId())
+                .name(user.getName())
                 .build();
     }
 }
